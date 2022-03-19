e4d0e254bd21317cfef9c861df75a9fa8533aef3
Shervin the Prodigy <68794773+Shervi28@users.noreply.github.com>
2022-03-19 14:11:17 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import streamlit as st
 
-st.write("Welcome to dineGraph")
+st.write("# Welcome to dineGraph")
 
+food = st.text_input("What have you eaten today? ")
+calories = st.text_input("Calorie of the food: ")
+
+if food and calories is None:
+    st.write("Nothing written")
+else:
+    st.write(f"Food: {food} , Calories of {food}: {calories}")
